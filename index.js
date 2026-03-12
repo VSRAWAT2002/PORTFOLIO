c00fce77c606b2728e882825313b03ddf278f2ab
Vijay <vijaysingh.rawat2002@gmail.com>
2026-03-12 16:34:10 +0530

    uploaded

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ controlOfContactForm.addEventListener("submit", (e) => {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
-    let date = new Date().toLocaleString(); 
+    let date = new Date().toLocaleString();
 
     let response = {
         name,
@@ -68,3 +68,44 @@ function showUserResponses() {
         controlOfUserResponses.append(newDiv);
     });
 }
+
+var form = document.getElementById("contact-me");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                status.innerHTML =
+                    "Thanks! Your message has been sent successfully.";
+                status.style.color = "green";
+                form.reset(); //form clearing
+                response.json().then((data) => {
+                    if (Object.hasOwn(data, "errors")) {
+                        status.innerHTML = data["errors"]
+                            .map((error) => error["message"])
+                            .join(", ");
+                    } else {
+                        status.innerHTML =
+                            "Oops! There was a problem submitting your form";
+                    }
+                });
+            }
+        })
+        .catch((error) => {
+            status.innerHTML =
+                "Oops! There was a problem connecting to the server";
+            status.style.color = "red";
+        });
+}
+
+form.addEventListener("submit", handleSubmit);
